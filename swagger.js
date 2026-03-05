7c65fc344bd1d5471dbb8efa6796b087c76dbe2d
isama78 <isa21005@byui.edu>
2026-03-05 14:58:13 -0300

    taking https off from the base url

--- swagger.js
+++ swagger.js
@@ -1,17 +1,17 @@
-import swaggerAutogen from 'swagger-autogen';
+import swaggerAutogen from "swagger-autogen";
 
 const doc = {
-    info: {
-        title: 'CSE341 Project 1',
-        version: '1.0.0',
-        description: 'API for CSE341 Project 1',
-    },
-    host: 'cse341-project1-4z97.onrender.com',
-    basePath: '/',
-    schemes: ['https'],
+  info: {
+    title: "CSE341 Project 1",
+    version: "1.0.0",
+    description: "API for CSE341 Project 1",
+  },
+  host: "cse341-project1-4z97.onrender.com",
+  basePath: "/",
+  schemes: ["https"],
 };
 
-const outputFile = './swagger.json';
-const endpointFiles = ['src/app.js'];
+const outputFile = "./swagger.json";
+const endpointFiles = ["src/app.js"];
 
 swaggerAutogen(outputFile, endpointFiles, doc);
