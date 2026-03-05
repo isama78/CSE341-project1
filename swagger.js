88deac048a815cb546aa5cd774e5671962b6fd66
isama78 <isa21005@byui.edu>
2026-03-05 14:51:07 -0300

    taking https off from the base url

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         version: '1.0.0',
         description: 'API for CSE341 Project 1',
     },
-    host: 'https://cse341-project1-4z97.onrender.com/',
+    host: 'cse341-project1-4z97.onrender.com',
     basePath: '/',
     schemes: ['https'],
 };
